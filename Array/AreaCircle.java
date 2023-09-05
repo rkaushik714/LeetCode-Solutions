ffa46940ae52e0dc1db77d2324d2a4ce0f0b8bdd
Rahul Kaushik <93032345+rkaushik714@users.noreply.github.com>
2023-09-05 20:56:50 +0530

    Add files via upload

---
+++ Array/AreaCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AreaCircle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         double r = sc.nextDouble();
+         double result = 3.14*r*r; 
+         System.out.println(result); 
+
+         sc.close();
+    }    
+}
